8275176c62a38fe6e50a3526c79a73d6d51c6549
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-05-15 22:51:07 +0530

    Add files via upload

---
+++ single-thread.js
@@ -0,0 +1,11 @@
+setTimeout(() => {
+    console.log("hello rahul");
+
+    
+}, 2000);
+setTimeout(() => {
+    console.log("welcome to rsg developer");
+
+    
+}, 2000);
+console.log("automatic run the program");
