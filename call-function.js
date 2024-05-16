4cdbd167274366484aeb1df0615e70fbddb2953e
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-05-16 20:51:00 +0530

    Add files via upload

---
+++ call-function.js
@@ -0,0 +1,13 @@
+function hello(){
+
+    console.log("Hello");
+
+}
+function demo(){
+    console.log("call the hello function")
+    hello();
+
+}
+console.log("call the demo function")
+demo();
+console.log("done ,bye!");
