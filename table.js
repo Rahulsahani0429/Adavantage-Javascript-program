966d516c91da1fec5fe183c4a817ba040c240af0
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-05-27 21:21:06 +0530

    Add files via upload

---
+++ table.js
@@ -0,0 +1,9 @@
+let t=document.body.firstElementChild.firstElementChild.firstElementChild//use of the this propeties we can access the child of the boby, html, etc
+console.log(t);
+console.log(t.rows);
+console.log(t.tHead);
+ console.log(t.tBodies);
+ 
+ 
+// console.log(t.tablcaption)
+// //console.log(tabl.rows)
