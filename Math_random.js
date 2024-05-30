1426d8a754b496a129d484c661cb0a46421ce96e
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-05-30 21:34:08 +0530

    Add files via upload

---
+++ Math_random.js
@@ -0,0 +1,4 @@
+let random=Math.random()+2;//this is use to  generate the random value according to the range with the decimal numve like 0.33,.23etc
+console.log(random);
+let nGenerate=Math.floor(Math.random()*5+4);//this is use to  generate the random value according to the range  without the decimal like a2,4,5,6 etc
+console.log(nGenerate);
