e5e52a8fb80e0b061530fd696635755644f2483a
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-06-01 23:08:13 +0530

    Add files via upload

---
+++ asycro_syncron.js
@@ -0,0 +1,7 @@
+let a=prompt("what is your favourt food");
+let b=prompt("what is your  age");
+let c=prompt("what is your favourt game");
+// let d=prompt("what is your favourt language");
+console.log(a+  +b+  +c);
+
+
