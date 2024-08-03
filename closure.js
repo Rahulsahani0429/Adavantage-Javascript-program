ee12217333228610ddd8825184a25d3ed4835256
Rahulsahani0429 <142796638+Rahulsahani0429@users.noreply.github.com>
2024-08-03 21:40:25 +0530

    Add files via upload

---
+++ closure.js
@@ -0,0 +1,19 @@
+const a=()  => {
+    let x=1
+    console.log(x)
+    const b=() => {
+        //let x= 4
+        console.log(x)
+    
+    const c=() => {
+        let x= 8
+        console.log(x)
+    }
+    c()
+   
+}
+x=998
+b()
+
+}
+a()
---
+++ cookie.js
@@ -0,0 +1,5 @@
+console.log(document.cookie);
+document.cookie="name=rahul112233300";
+document.cookie="name3=rahul11223330088";
+console.log(document.cookie);
+
